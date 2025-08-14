4d90758b135a928d325e26fd4cff4f1f574cb697
Mohd Wasim <pydwasim@gmail.com>
2025-08-14 21:11:05 +0530

    add files

---
+++ Js/factorial.js
@@ -0,0 +1,3 @@
+const Factorial = n => {
+    let fact = n.reduce( )
+}
---
+++ Projects/FaultyCalc/script.js
@@ -0,0 +1,17 @@
+let a = prompt("Enter your First Number")
+let b = prompt("Choose Operator + , - , * , / ")
+let c = prompt("Enter your Second Number")
+let random = Math.random();
+let obj = {
+    "+": "-",
+    "-": "*",
+    "*": "+",
+    "/": "%",
+}
+if (random > 0.1) {
+    alert(`The Result is ${eval(`${a} ${b} ${c}`)}`)
+}
+else {
+    b = obj[b];
+    alert(`The Result is ${eval(`${a} ${b} ${c}`)}`)
+}
